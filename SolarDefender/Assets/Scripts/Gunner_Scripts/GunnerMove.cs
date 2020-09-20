5c1f1d9c9ec89cdafd981c19bac28ac01aa227d5
Gonçalo Peres <goncalorcperes@hotmail.com>
2020-09-20 04:08:03 +0100

    Small changes

--- SolarDefender/Assets/Scripts/Gunner_Scripts/GunnerMove.cs
+++ SolarDefender/Assets/Scripts/Gunner_Scripts/GunnerMove.cs
@@ -1,42 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunnerMove : MonoBehaviour
 {
+
+ //   private Rigidbody2D rb;
+ //   private float jumpForce = 40f;
+ //   private bool engineIsOn;
+
+ //   public Button JetpackButton;
+
+ //   [SerializeField]
+ //   //private GameObject fire;
     
-	void Start ()
-    {
-		
-	}
+	//void Start ()
+ //   {
+ //       engineIsOn = false;
+ //       //fire.SetActive(false);
+ //       rb = GetComponent<Rigidbody2D>();
+ //       JetpackButton = GetComponent<Button>();
+ //   }
 	
-    //Input
-	void Update ()
-    {
-        ActivateThursters();
-    }
+ //   //Input
+	//void Update ()
+ //   {
+ //       CheckThursters();
+ //   }
 
-    //actions
-    void FixedUpdate()
-    {
-        CheckThursters();
-    }
+ //   //actions
+ //   void FixedUpdate()
+ //   {
+ //       //CheckThursters();
 
-    void CheckThursters()
-    {
-        if()
-        {
+ //       switch (engineIsOn)
+ //       {
+ //           case true:
+ //               rb.addForce(new Vector2(0f, jumpForce), ForceMethod2D.Force);
+ //               break;
+ //           case false:
+ //               rb.addForce(new Vector2(0f, 0f), ForceMethod2D.Force);
+ //               break;
+ //       }
+ //   }
 
-        }
-        else
-        {
+ //   void CheckThursters()
+ //   {
+ //       if (Input.GetButtonDown("JetpackButton"))
+ //       {
+ //           engineIsOn = true;
+ //       }
+ //       if (Input.GetButtonUp("JetpackButton"))
+ //       {
+ //           engineIsOn = false;
+ //       }
+ //   }
 
-        }
+    //void ActivateThursters()
+    //{
+    //    rb.addForce(new Vector2(0f, jumpForce), ForceMethod2D.Force);
+    //}
 
-    }
-
-    void ActivateThursters()
-    {
-
-    }
+    //void DeactivateThursters()
+    //{
+    //    rb.addForce(new Vector2(0f, 0f), ForceMethod2D.Force);
+    //}
 }
